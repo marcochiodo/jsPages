a419b1520cdb3aea7517635c2e7a154e58b99579
marcochiodo <marcochio94@gmail.com>
2013-10-30 11:14:05 +0100

    Implemented position of video background

--- jsPagesVideoAdapt.js
+++ jsPagesVideoAdapt.js
@@ -1,7 +1,11 @@
+/*
 
+	Edit 2
+
+*/
 (function($) {
 
-	$.fn.jsPagesVideoAdapt = function( parent ){
+	$.fn.jsPagesVideoAdapt = function( parent , top , left ){
 	
 		var w = this.width();
 		var h = this.height();
@@ -19,15 +23,15 @@
 	
 		$(window).resize(function(){
 		
-			_this.jsPagesVideoAdaptSet(parent , w , h );
+			_this.jsPagesVideoAdaptSet(parent , w , h , top , left );
 		
 		});
 		
-		this.jsPagesVideoAdaptSet(parent , w , h );
+		this.jsPagesVideoAdaptSet(parent , w , h , top , left );
 	
 	};
 	
-	$.fn.jsPagesVideoAdaptSet = function( parent , w , h ){
+	$.fn.jsPagesVideoAdaptSet = function( parent , w , h , top , left ){
 	
 		var parentW = $(parent).width();
 		var parentH = $(parent).height();
@@ -57,6 +61,54 @@
 			width : newW,
 			height : newH
 		});
+		
+		var css = {};
+		var maxDX = newW-parentW; //Disassamento X
+		var maxDY = newH-parentH; //Disassamento Y
+		
+		switch(top){
+			case 'top':css.top='0%';break;
+			case 'center':css.top='-'+((newH-parentH)/2)+'px';break;
+			case 'bottom':css.bottom='0%';break;
+			default:
+				if( top.indexOf('%') != -1 ){
+					top = parseInt(top);
+					top = newH*(top/100);
+					if( top > maxDY )
+						top = maxDY;
+					css.top = '-'+top+'px';
+				}
+				else{
+					top = parseInt(top);
+					if( top > maxDY )
+						top = maxDY;
+					css.top = '-'+top+'px';
+				}
+			break;
+		}
+		switch(left){
+			case 'left':css.left='0%';break;
+			case 'center':css.left='-'+((newW-parentW)/2)+'px';break;
+			case 'right':css.right='0%';break;
+			default:
+				if( left.indexOf('%') != -1 ){
+					left = parseInt(left);
+					left = newW*(left/100);
+					if( left > maxDX )
+						left = maxDX;
+					css.left = '-'+left+'px';
+				}
+				else{
+					left = parseInt(left);
+					if( left > maxDX )
+						left = maxDX;
+					css.left = '-'+left+'px';
+				}
+			break;
+		}
+	
+		this.css( css );
+	
 	
 	}
 		
