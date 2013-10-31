b6ab0a8d5c5182e03827d0622f89f57c32dce6d2
Marco Chiodo <marcochio94@gmail.com>
2013-10-31 12:46:37 +0100

    Add utility file. Insert function getParentOfSize()

---
+++ jsPagesUtility.js
@@ -0,0 +1,19 @@
+(function($) {
+	
+	$.fn.jsPagesGetParentOfSize = function(){
+		var e = $(this);
+		if( e.css('position') == 'absolute' || e.css('position') == 'fixed' ){
+		
+			var parent = e.parent();
+			while( parent.css('position') == 'static' && parent.get(0).tagName != 'BODY' )
+				parent = parent.parent();
+		}
+		else
+			var parent = e.parent();
+
+		
+		return parent;
+		
+	}    
+  
+})(jQuery);
