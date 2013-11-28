64d82cde26d66260df239c07b2eaa58cb1973e44
Marco Chiodo <marcochio94@gmail.com>
2013-11-28 10:40:41 +0100

    Add function GetPosiion(). It return x and y absolute position in the page

--- jsPagesUtility.js
+++ jsPagesUtility.js
@@ -13,5 +13,31 @@
 
 		return parent;
 	}
+	
+	$.fn.jsPagesGetPosition = function(){
+	
+		var pos=[];
+		pos['left']=0;
+		pos['top']=0;
+		obj = this.get(0);
+
+		if(obj) {
+			while(obj.offsetParent){
+				pos['left']+=obj.offsetLeft-obj.scrollLeft;
+				pos['top']+=obj.offsetTop-obj.scrollTop;
+				var tmp=obj.parentNode;
+				while(tmp!=obj.offsetParent){
+					pos['left']-=tmp.scrollLeft;
+					pos['top']-=tmp.scrollTop;
+					tmp=tmp.parentNode;
+				}
+				obj=obj.offsetParent;
+			}
+			pos['left']+=obj.offsetLeft;
+			pos['top']+=obj.offsetTop;
+		}
+		return {x:pos['left'],y:pos['top']};
+	
+	}
 
 })(jQuery);
