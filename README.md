73a1b89dfeb2899c810e0fa282b59e06685568f0
noise- <riccardobergonzo@gmail.com>
2013-12-26 21:36:39 +0100

    animations now use css3 transitions

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ jsPages
 Version 0.2
 
 A lightweight jQuery plugin to simplify websites developement
+
+Events
+=======
+
+ - the 'jsPagesLoad' event is fired everytime a link is selected and
+   it is found in the init array
+
+ - the 'jsPagesDone' event id fired everytime an animation in finished
--- jsPages.js
+++ jsPages.js
@@ -1,82 +1,295 @@
 Array.prototype.size = function() { return this.filter( function( a ){ return a !== undefined; } ).length };
 
-(function($) {
+( function( $ ) {
 
-	$.jsPagesInit = function( pages, current ) {
+	// init
+
+	$.jsPagesInit = function( pages, cont ) {
+
+		for( var i = 0; i < pages.size(); ++i )
+			pages[i].n = i;
 
 		$.jsPagesList = pages;
-		$.jsPagesCurrent = pages[ current ];
+
+		$( cont + ' div' ).each( function( i, id ) {
+
+			if( $( id ).css( 'display' ) != 'none' )
+				$.jsPagesActual = $.jsPagesList[i];
+		} );
+
+		$( cont ).css( { 'overflow' : 'hidden' } );
+
+		$.jsPagesCssAnim = $.jsPagesTransition();
+		$.jsPagesCssTransform = $.jsPagesTransform();
 	};
 
-	// set to be executed on click event
+	// exit link
 
 	$.fn.jsPagesGetExitLink = function( e ) {
 
-		for( var i = 0; i < $.jsPagesList.size(); ++i ) {
+		if( history.pushState ) {
 
-			if( $( this ).attr( 'href' ) == $.jsPagesList[i].l ) {
+			for( var i = 0; i < $.jsPagesList.size(); ++i ) {
 
-				e.preventDefault();
-				$.event.trigger({
-					type : 'jsPagesLoad',
-					id : $.jsPagesList[i].id,
-					link : $.jsPagesList[i].l,
-					n : $.jsPagesList[i].n
-				});
-				break;
+				if( $( this ).attr( 'href' ).split('/').pop() == $.jsPagesList[i].l.split('/').pop() ) {
+
+					e.preventDefault();
+
+					if( $.jsPagesList[i].n != $.jsPagesActual.n ) {
+
+						$.event.trigger( {
+							type : 'jsPagesLoad',
+							id : $.jsPagesList[i].id,
+							link : $( this ).attr( 'href' ),
+							n : $.jsPagesList[i].n
+						} );
+					}
+
+					break;
+				}
 			}
 		}
 	};
 
-	// sliding animation
+	// get css3 transition type
 
-	$.jsPagesAnimSlide = function( e ) {
+	$.jsPagesTransition = function() {
+		var style = document.body.style || document.documentElement.style;
 
-		if( e.n < $.jsPagesCurrent.n ) {
-			$( $.jsPagesCurrent.id ).css({ display : 'inline-block', left : '0%' });
-			$( e.id ).css({ display : 'inline-block', left : '0%' });
-			$( e.id ).animate({ left : '25%' }, 1000 );
-			$( $.jsPagesCurrent.id ).animate({ left : '25%' }, 1000 );
-			setTimeout( function(){
-			$( $.jsPagesCurrent.id ).hide(0);
-			$.jsPagesCurrent = e;
-			}, 1000 );
-		}
-		else if( e.n > $.jsPagesCurrent.n ) {
-			$( $.jsPagesCurrent.id ).css({ display : 'inline-block', left : '25%' });
-			$( e.id ).css({ display : 'inline-block', left : '25%' });
-			$( e.id ).animate({ left : '0%' }, 1000 );
-			$( $.jsPagesCurrent.id ).animate({ left : '0%' }, 1000 )
-			setTimeout( function(){
-			$( e.id ).css({ left : '25%' });
-			$( $.jsPagesCurrent.id ).hide(0);
-			$.jsPagesCurrent = e;
-			}, 1000 );
-		}
+		if( style.WebkitTransition !== undefined )
+			return '-webkit-transition';
+		else if( style.MozTransition !== undefined )
+			return '-moz-transition';
+		else if( style.MsTransition !== undefined )
+			return '-ms-transition';
+		else if( style.OTransition !== undefined )
+			return '-o-transition';
+		else if( style.transition !== undefined )
+			return 'transition';
+		else
+			return false;
+	};
+
+	// get css3 transform type
+
+	$.jsPagesTransform = function() {
+		var style = document.body.style || document.documentElement.style;
+
+		if( style.webkitTransform !== undefined )
+			return '-webkit-transform';
+		else if( style.msTransform !== undefined )
+			return '-ms-transform';
+		else if( style.MozTransform !== undefined )
+			return '-moz-transform';
+		else if( style.OTransform !== undefined )
+			return '-o-transform';
+		else if( style.transform !== undefined )
+			return 'transform';
+		else
+			return false;
+	};
+
+	// anim utility
+
+	$.jsPagesAnim = function( id, property, value, duration, easing ) {
+
+		easing = easing || 'ease';
+
+		if( $.jsPagesCssAnim !== false ) {
+
+			$( id ).css( $.jsPagesCssAnim, ( property + ' ' + easing + ' ' + ( duration - 10 ) / 1000 + 's' ) );
+
+			setTimeout( function() {
+				$( id ).css( property, value );
+			}, 10 );
+
+			setTimeout( function() {
+				$( id ).css( $.jsPagesCssAnim, ( property + ' ' + 0 + 's' ) );
+			}, duration / 2 );
+
+		} else
+			$( id ).animate( { property : value }, duration );
+	}
+
+	// simple fallback anim
+
+	$.jsPagesAnimFallback = function( e ) {
+
+		$( $.jsPagesActual.id ).hide();
+		$( e.id ).show();
+
+		$.jsPagesActual = e;
+
+		$.event.trigger( {
+			type : 'jsPagesDone',
+			id : e.id
+		} );
 	};
 
 	// fading animation
 
 	$.jsPagesAnimFade = function( e ) {
 
-		$( $.jsPagesCurrent.id ).hide( 500 );
+		$( $.jsPagesActual.id ).hide( 500 );
 		$( e.id ).show( 500 );
 
-		$.jsPagesCurrent = e;
+		$.jsPagesActual = e;
+
+		$.event.trigger( {
+			type : 'jsPagesDone',
+			id : e.id
+		} );
+	};
+
+	// sliding animation
+
+	$.jsPagesAnimSlide = function( e ) {
+
+		if( $.jsPagesBusy != true ) {
+
+			$.jsPagesBusy = true;
+
+			if( e.n < $.jsPagesActual.n ) {
+
+				$( $.jsPagesActual.id ).css( 'left', '0%' );
+				$( e.id ).css( 'display', 'inline-block' );
+
+				$.jsPagesAnim( e.id, 'left', '25%', 1000 );
+				setTimeout( function(){ $.jsPagesAnim( $.jsPagesActual.id, 'left', '25%', 1000 ) }, 10 );
+
+				setTimeout( function() {
+
+					$( $.jsPagesActual.id ).hide();
+					$.jsPagesActual = e;
+					$.jsPagesBusy = false;
+
+					$.event.trigger( {
+						type : 'jsPagesDone',
+						id : e.id
+					} );
+
+				}, 1000 );
+			}
+
+			else if( e.n > $.jsPagesActual.n ) {
+
+				$( e.id ).css( 'left', '25%' );
+				$( e.id ).css( 'display', 'inline-block' );
+
+				$.jsPagesAnim( $.jsPagesActual.id, 'left', '0%', 1000 );
+				$.jsPagesAnim( e.id, 'left', '0%', 1000 );
+
+				setTimeout( function() {
+
+					$( $.jsPagesActual.id ).hide();
+					$( e.id ).css( 'left', '25%' );
+
+					$.jsPagesActual = e;
+					$.jsPagesBusy = false;
+
+					$.event.trigger( {
+						type : 'jsPagesDone',
+						id : e.id
+					} );
+
+				}, 1000 );
+			}
+		}
 	};
 
 	// flipping animation
 
 	$.jsPagesAnimFlip = function( e ) {
 
-		$( $.jsPagesCurrent.id ).css({ '-webkit-transform' : 'rotateY(90deg)', 'transform' : 'rotateY(90deg)' });
-		$( $.jsPagesCurrent.id ).delay( 500 ).hide( 0 );
-		$( e.id ).delay( 500 ).show( 0 );
-		setTimeout( function() { $( e.id ).css({ '-webkit-transform' : 'rotateY(0deg)', 'transform' : 'rotateY(0deg)' }); }, 500 );
-		$.jsPagesCurrent = e;
+		if( $.jsPagesCssTransform != false ) {
+
+			if( $.jsPagesBusy != true ) {
+
+				$.jsPagesBusy = true;
+
+				$( $.jsPagesActual.id ).css( $.jsPagesCssTransform, 'rotateY(0deg)' );
+				$( e.id ).css( $.jsPagesCssTransform, 'rotateY(90deg)' );
+
+				$.jsPagesAnim( $.jsPagesActual.id, $.jsPagesCssTransform, 'rotateY(90deg)', 500, 'linear' );
+
+				setTimeout( function() {
+
+					$( $.jsPagesActual.id ).hide();
+					$( e.id ).show();
+					$.jsPagesAnim( e.id, $.jsPagesCssTransform, 'rotateY(180deg)', 500, 'linear' );
+
+					setTimeout( function() {
+
+						$( e.id ).css( $.jsPagesCssTransform, 'rotateY(0deg)' );
+						$.jsPagesActual = e;
+						$.jsPagesBusy = false;
+
+						$.event.trigger( {
+							type : 'jsPagesDone',
+							id : e.id
+						} );
+
+					}, 501 );
+
+				}, 501 );
+			}
+
+		} else
+
+			$.jsPagesAnimFallback( e );
 	};
 
-})(jQuery);
+	// skewing animation
+
+	$.jsPagesAnimSkew = function( e ) {
+
+		if( $.jsPagesCssTransform != false ) {
+
+			if( $.jsPagesBusy != true ) {
+
+				$.jsPagesBusy = true;
+
+				$( $.jsPagesActual.id ).css( $.jsPagesCssTransform, 'skewY(0deg)' );
+				$( e.id ).css( $.jsPagesCssTransform, 'skewY(90deg)' );
+
+				$.jsPagesAnim( $.jsPagesActual.id, $.jsPagesCssTransform, 'skewY(90deg)', 500, 'linear' );
+
+				setTimeout( function() {
+
+					$( $.jsPagesActual.id ).hide();
+					$( e.id ).show();
+					$.jsPagesAnim( e.id, $.jsPagesCssTransform, 'skewY(180deg)', 500, 'linear' );
+
+					setTimeout( function() {
+
+						$( e.id ).css( $.jsPagesCssTransform, 'skewY(0deg)' );
+						$.jsPagesActual = e;
+						$.jsPagesBusy = false;
+
+						$.event.trigger( {
+							type : 'jsPagesDone',
+							id : e.id
+						} );
+
+					}, 501 );
+
+				}, 501 );
+			}
+
+
+		} else
+
+			$.jsPagesAnimFallback( e );
+	};
+
+	$.jsPagesPushHistory = function( e ) {
+
+		if( $.jsPagesBusy != true ) {
+			history.pushState( {}, '', e.link );
+		}
+	};
+
+} )( jQuery );
 
 // bindings
 
